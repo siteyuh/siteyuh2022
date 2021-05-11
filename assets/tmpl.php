c559597c3ac065f3585827c1cc3541e266a301cf
kity <yuhei.kitano@gmail.com>
2021-05-11 15:59:10 +0900

    ベースのHTMLをテンプレート化

---
+++ assets/tmpl.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+include 'assets/twittercard.php'; ?>
+  <title><?=$title?></title>
+  <link rel="stylesheet" href="/assets/bind.css">
+  <link rel="canonical" href="https://siteyuh.com<?=$_SERVER['REQUEST_URI']?>">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-7G89FV891V"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7G89FV891V');
+  </script>
+</head>
+
+<body>
+  <div class="thecontent">
+    <header class="sticky flex" id="global">
+      <h1 class="invisible">yuhの自己紹介サイト</h1>
+      <div class="logo"><a href="/"><img src="/assets/logo.svg" alt="site logo"></a></div>
+      <div class="menu" onclick="javascript:toggle_menu()"><img src="/assets/menu.svg" alt="menu" name="menu_button"></div>
+    </header>
+
+    <main>
+  
+    </main>
+    
+    <footer>
+      <p class="white small">
+        Copyright All Rights Reserved.
+      </p>
+    </footer>
+  </div>
+  
+  <!-- オーバーレイするメニュー -->
+  <nav id="globalnav">
+    <ul class="nav-container">
+      <li><a href=""></a></li>
+    </ul>
+  </nav>
+  
+  <script src="/assets/main.js"></script>
+</body>
+
+</html>
--- assets/twittercard.php
+++ assets/twittercard.php
@@ -1,17 +1,5 @@
 <?php
-if ($pageid == 'gallery') {
-  # gallery page's twitter card
-  $title = '';
-  $descri = '';
-  $picurl = '';
-  $type = 'article';
-} else {
-  # homepage's or other twitter card
-  $title = '';
-  $descri = '';
-  $picurl = '';
-  $type = 'website';
-}
+
 $selfurl = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 echo '<meta name="twitter:card" content="summary">'.PHP_EOL;
