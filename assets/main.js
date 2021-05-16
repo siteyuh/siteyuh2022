343939584cdb145101df7e5cd718db838e50ee59
kity <yuhei.kitano@gmail.com>
2021-05-16 11:09:45 +0900

    Topデザイン

--- assets/main.js
+++ assets/main.js
@@ -17,3 +17,9 @@ function thumb_click(photopath, descri) {
   }
   document.getElementById("fig_caption").innerHTML = descri;
 }
+
+/* https://coliss.com/articles/build-websites/operation/css/viewport-units-on-mobile.html */
+// 最初に、ビューポートの高さを取得し、0.01を掛けて1%の値を算出して、vh単位の値を取得
+let vh=window.innerHeight * 0.01;
+// カスタム変数--vhの値をドキュメントのルートに設定
+document.documentElement.style.setProperty('--vh',`${vh}px`);
--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 $pageid = 'home';
 
+include 'assets/conn.php';
+$cat_sql = "SELECT * FROM `cat`";
+
+if ($statement = $database_handler->prepare($cat_sql)) {
+  $statement->execute();
+  $categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $maincontent .= '<ul class="category_images">'.PHP_EOL;
+  foreach ($categories as $cat) {
+    // strstr($string, '.co', true)
+    $maincontent .= '<li class="cat';
+    $maincontent .= $cat['categoryid'];
+    $maincontent .= '"><a href="gallery.php?catid=';
+    $maincontent .= $cat['categoryid'];
+    $maincontent .= '"><img src="';
+    $maincontent .= strstr($cat['catphotopath'], '=', true).'=s156-c';
+    $maincontent .= '" alt="';
+    $maincontent .= $cat['jdescribe'];
+    $maincontent .= '"></a></li>'.PHP_EOL;
+    $maincontent .= '';
+  }
+  $maincontent .= '</ul>'.PHP_EOL;
+}
+
 /* TwitterCard */
 $title = 'yuhのエッチなフォトギャラリー';
 $descri = 'エッチなフォトギャラリーを観てください';
 $picurl = 'https://siteyuh.com/img/me.jpg';
 $type = 'website';
 
-$maincotent = <<< EOM
-
-EOM;
 
 include 'assets/tmpl.php';
